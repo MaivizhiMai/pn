2fc39f2ad57082f36d117a8302dbf3ba9d554696
MaivizhiMai <nmaivizhi@gmail.com>
2016-09-17 05:27:08 +0530

    Create Pro27.java

---
+++ Pro27.java
@@ -0,0 +1,31 @@
+
+
+
+public class C
+{
+   public static int strToInt( String str )
+   {
+        
+    int i = 0;
+    int num = 0;
+    boolean isNeg = false;
+
+   
+    if (str.charAt(0) == '-') 
+    {
+        isNeg = true;
+        i = 1;
+    }
+
+    //Process each character of the string;
+    while( i < str.length())
+    {
+        num *= 10;
+        num += str.charAt(i++) - '0'; //Minus the ASCII code of '0' to get the value of the charAt(i++).
+    }
+
+    if (isNeg)
+        num = -num;
+    return num;
+}
+}
