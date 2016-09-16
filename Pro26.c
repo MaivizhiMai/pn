6601352fc0ca8a9f51fb24c5ce946eb2a9477acf
MaivizhiMai <nmaivizhi@gmail.com>
2016-09-17 05:25:03 +0530

    Create Pro26.c

---
+++ Pro26.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+	void main() {
+	  char t[100];
+	  printf("Please enter strings:");
+
+	   gets(t);
+
+	  while (strlen(t) >= 1){
+
+
+
+
+
+	  if(strchr(text,"bbb")!= NULL)
+
+
+
+	   printf("The string is : %s\n",t);
+
+
+
+
+
+
+
+	     gets(t);
+
+
+
+	  }
+
+
+
+	}
