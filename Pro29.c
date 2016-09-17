44697eb23eaf8d75376da5ffc02d42d944f30e7a
MaivizhiMai <nmaivizhi@gmail.com>
2016-09-17 05:32:15 +0530

    Create Pro29.c

---
+++ Pro29.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+double temp,temp1;
+//float median;
+int i,c=0, m=0;,log2;
+clrscr();
+scanf("%lf",&temp);
+temp1=1<<log2
+  {
+c++;
+}
+}
+m = pow(2,(1<<(count/2)));
+printf("m:%f",m);
+getch();
+}
